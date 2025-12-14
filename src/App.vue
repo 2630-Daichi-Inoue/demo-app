f646647531e40fd1a246ee3c71168469c8514692
2630-Daichi-Inoue <inouedaichi777@gmail.com>
2025-12-14 18:52:00 +0900

    lesson 73

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+
+</script>
+
+<template>
+  <main>
+    <h1>今日何食べる？</h1>
+  </main>
+</template>
+
+<style scoped>
+  main {
+    width: 350px;
+    margin: 100px auto;
+    text-align: center;
+  }
+</style>
